712b3074a1791a028a661209e162201ddd1ef2dd
Numa Maurin <numa.maurin@teads.tv>
2022-04-29 17:13:32 +0200

    add border and new way for state

--- example/lib/integration_list.dart
+++ example/lib/integration_list.dart
@@ -6,71 +6,112 @@ class integration_list extends StatefulWidget {
 }
 
 class _integration_listState extends State<integration_list> {
-  bool _isPressed = false;
-
-  void _ButtonFunction() {
-    setState(() {
-      if (_isPressed) {
-        _isPressed = false;
-        print(_isPressed);
-      } else {
-        _isPressed = true;
-        print(_isPressed);
-      }
-    });
-  }
+  String _integrationType = "ScrollView";
 
   @override
   Widget build(BuildContext context) {
     return SliverToBoxAdapter(
-        child: Container(
-            child: Row(
-      children: [
-        SizedBox(width: 50),
-        Column(
+      child: Container(
+        child: Row(
           children: [
-            Container(
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(5.0),
-                  )),
-              child: Column(children: [
-                SizedBox(height: 10, width: 50),
-                IconButton(
-                  icon: Image.asset(
-                      "assets/ScrollView.imageset/ScrollView150.png"),
-                  iconSize: 100,
-                  onPressed: () {},
+            SizedBox(width: 50),
+            Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5.0),
+                      )),
+                  child: Column(children: [
+                    SizedBox(height: 10),
+                    IconButton(
+                      icon: Image.asset(
+                          "assets/ScrollView.imageset/ScrollView150.png"),
+                      iconSize: 100,
+                      onPressed: () {
+                        setState(() {
+                          _integrationType = "ScrollView";
+                        });
+                      },
+                    ),
+                    Text("ScrollView"),
+                    SizedBox(height: 10),
+                  ]),
                 ),
-                Text("ScrollView"),
-                SizedBox(height: 10),
-              ]),
+                Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5.0),
+                      )),
+                  child: Column(children: [
+                    SizedBox(height: 10),
+                    IconButton(
+                      icon: Image.asset(
+                          "assets/CollectionView.imageset/CollectionView150.png"),
+                      iconSize: 100,
+                      onPressed: () {
+                        setState(() {
+                          _integrationType = "CollectionView";
+                        });
+                      },
+                    ),
+                    Text("CollectionView"),
+                    SizedBox(height: 10),
+                  ]),
+                ),
+              ],
             ),
-            IconButton(
-              icon: Image.asset(
-                  "assets/CollectionView.imageset/CollectionView150.png"),
-              iconSize: 100,
-              onPressed: () {},
-            ),
+            SizedBox(width: 50),
+            Column(children: [
+              Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(5.0),
+                    )),
+                child: Column(children: [
+                  SizedBox(height: 10),
+                  IconButton(
+                    icon: Image.asset(
+                        "assets/TableView.imageset/TableView150.png"),
+                    iconSize: 100,
+                    onPressed: () {
+                      setState(() {
+                        _integrationType = "TableView";
+                      });
+                    },
+                  ),
+                  Text("TableView"),
+                  SizedBox(height: 10),
+                ]),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(5.0),
+                    )),
+                child: Column(children: [
+                  SizedBox(height: 10),
+                  IconButton(
+                    icon: Image.asset("assets/WebView.imageset/WebView150.png"),
+                    iconSize: 100,
+                    onPressed: () {
+                      setState(() {
+                        _integrationType = "WebView";
+                      });
+                    },
+                  ),
+                  Text("Webview"),
+                  SizedBox(height: 10),
+                ]),
+              ),
+            ]),
           ],
         ),
-        SizedBox(width: 50),
-        Column(children: [
-          SizedBox(height: 10),
-          IconButton(
-            icon: Image.asset("assets/TableView.imageset/TableView150.png"),
-            iconSize: 100,
-            onPressed: () {},
-          ),
-          SizedBox(height: 10),
-          IconButton(
-            icon: Image.asset("assets/WebView.imageset/WebView150.png"),
-            iconSize: 100,
-            onPressed: () {},
-          )
-        ]),
-      ],
-    )));
+      ),
+    );
   }
 }
